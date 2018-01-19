8058b91f59881b895fafdfaac33ba30565637c3b
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-01-19 14:43:00 -0500

    Created histogram for pokemon capture rate. Created scatterplot comparing pokemon capture rate with hatching time.

--- data-219/homework0/homework0.py
+++ data-219/homework0/homework0.py
@@ -7,4 +7,15 @@ hatch_time = poke_species.hatch_counter
 hatch_time_hist = hatch_time.plot(kind='hist', bins=20, title='Pokemon Gestation Time')
 hatch_time_hist.set_xlabel("Hatching Time")
 hatch_time_hist.set_ylabel("Hatching Time Frequency")
+
+catch_rate = poke_species.capture_rate
+catch_rate_hist = catch_rate.plot(kind='hist', bins=13, title='Pokemon Capture Rate')
+catch_rate_hist.set_xlabel("Capture Rate")
+catch_rate_hist.set_ylabel("Capture Rate Frequency")
+plt.close()
+
+plt.scatter(hatch_time, catch_rate)
+plt.title('Pokemon Gestation Time and Capture Rate')
+plt.xlabel("Gestation Time")
+plt.ylabel("Capture Rate")
 plt.show()
