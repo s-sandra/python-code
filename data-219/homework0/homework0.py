8acb5f5b235344600fdf599917772c90e040b2f9
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-01-19 12:06:28 -0500

    Loaded pokemon data into dataframe, formatted, and created histogram of hatching time.

--- data-219/homework0/homework0.py
+++ data-219/homework0/homework0.py
@@ -1,1 +1,10 @@
-print(0)
+import pandas as pd
+import matplotlib.pyplot as plt
+
+poke_species = pd.read_csv("pokemon_species.csv",index_col=1)
+poke_species = poke_species.fillna(0)
+hatch_time = poke_species.hatch_counter
+hatch_time_hist = hatch_time.plot(kind='hist', bins=20, title='Pokemon Gestation Time')
+hatch_time_hist.set_xlabel("Hatching Time")
+hatch_time_hist.set_ylabel("Hatching Time Frequency")
+plt.show()
