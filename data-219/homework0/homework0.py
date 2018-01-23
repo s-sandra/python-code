e108c47f268de3143b3590cec0ee5dae039fa7ac
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-01-23 18:21:42 -0500

    Added notches to narcissism based on gender boxplot and removed ages less than 14 on NPI data.

--- data-219/homework0/homework0.py
+++ data-219/homework0/homework0.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 poke_species = pd.read_csv("pokemon_species.csv",index_col=1)
 poke_species = poke_species.fillna(0)
 hatch_time = poke_species.hatch_counter
-hatch_time_hist = hatch_time.plot(kind='hist', bins=20, title='Pokemon Gestation Time')
-hatch_time_hist.set_xlabel("Hatching Time")
+hatch_time_hist = hatch_time.plot(kind='hist', bins=18, title='Pokemon Gestation Time')
+hatch_time_hist.set_xlabel("Hatching Time (egg cycles)")
 hatch_time_hist.set_ylabel("Hatching Time Frequency")
+plt.close()
 
 catch_rate = poke_species.capture_rate
 catch_rate_hist = catch_rate.plot(kind='hist', bins=13, title='Pokemon Capture Rate')
 catch_rate_hist.set_xlabel("Capture Rate")
 catch_rate_hist.set_ylabel("Capture Rate Frequency")
+plt.close()
 
 plt.scatter(hatch_time, catch_rate)
 plt.title('Pokemon Gestation Time and Capture Rate')
@@ -46,7 +48,7 @@ top_styles.plot(kind="bar", title="Ten Most Common Dress Styles")
 plt.xlabel("Dress Styles")
 plt.ylabel("Frequency of Dress Style")
 plt.tight_layout()
-plt.show()
+plt.close()
 
 price_by_rating = dresses[["Price","Rating"]]
 price_by_rating.boxplot(by="Price")
@@ -61,14 +63,14 @@ personality_scores.gender = personality_scores.gender.replace(1,"male")
 personality_scores.gender = personality_scores.gender.replace(2,"female")
 scores_by_gender = personality_scores[["gender","score"]]
 scores_by_gender = scores_by_gender[(scores_by_gender.gender == "female") | (scores_by_gender.gender == "male")]
-scores_by_gender.boxplot(by="gender")
+scores_by_gender.boxplot(by="gender",notch=True)
 plt.title("Narcissism Score Based On Gender")
 plt.xlabel("Gender")
 plt.ylabel("Score")
 plt.suptitle("")
 plt.close()
 
-age_errors = personality_scores[(personality_scores.age > 100) | (personality_scores.age < 9)].index
+age_errors = personality_scores[(personality_scores.age > 100) | (personality_scores.age < 14)].index
 purged_scores = personality_scores
 purged_scores.age = purged_scores.age.drop(age_errors)
 
