a741631fabb3714bdc4ab5ca152908b79624e7dc
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-02-21 20:38:12 -0500

    Merged students and schools dataframe. Counted number of students per area.

--- data-219/homework-2/homework2.py
+++ data-219/homework-2/homework2.py
@@ -10,14 +10,15 @@ gpas = gpas.dropna()
 gpas = gpas.rename(columns = {"value":"GPA"})
 gpas.year = gpas.year.str[3:]
 
+# removes duplicated schools, adds area column
 schools = everything[["school","type","mascot"]]
-del everything
-
+schools = schools.drop_duplicates()
 schools["area"] = np.repeat("rural", len(schools))
+del everything
 
 # UVA, Richmond and UMW all urban.
 urban_schools = np.where((schools.school == "UMW") | (schools.school == "Richmond") | (schools.school == "UVA"))
-for school in urban_schools:
+for school in urban_schools[0]:
     schools.set_value(school, "area", "urban")
 
 # removes duplicated students
@@ -34,3 +35,10 @@ for gpa in senior_gpas:
     sum_gpa += gpas.iloc[gpa].GPA
 avg_senior_gpa = sum_gpa / len(senior_gpas)
 print("avg senior gpa: ", avg_senior_gpa)
+
+students_and_schools = pd.merge(students, schools, on="school")
+students_and_schools = students_and_schools.drop_duplicates()
+rural_students = np.where(students_and_schools.area == "rural")
+urban_students = np.where(students_and_schools.area == "urban")
+print("number of urban students: ", len(urban_students[0]))
+print("number of rural students: ", len(rural_students[0]))
