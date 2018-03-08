486ccf79029a827ba611896491887c9bfe9c5062
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-03-08 16:07:59 -0500

    Plotted KDEs for year and mass columns.

--- data-219/homework-3/homework3.py
+++ data-219/homework-3/homework3.py
@@ -1,5 +1,7 @@
 import json
 import pandas as pd
+import scipy.stats
+import matplotlib.pyplot as plt
 
 # loads meterorite data into nasa_data object
 data = open("meteorites.json", encoding="utf8")
@@ -26,3 +28,23 @@ year_and_mass.year = year_and_mass.year.astype(int)
 # casts masses to float, converts from grams to pounds
 year_and_mass.mass = year_and_mass.mass.astype(float)
 year_and_mass.mass = round(year_and_mass.mass / 453.59237, 3)
+
+# calculates and plots KDE for years, with bandwidth of 3
+year_and_mass = year_and_mass.sort_values(by="year")
+kde = scipy.stats.gaussian_kde(year_and_mass.year, bw_method = 3)
+plt.plot(year_and_mass.year, kde(year_and_mass.year))
+plt.title("Distribution of Meteorite Landing Years")
+plt.xlabel("Year")
+plt.ylabel("Probability Density")
+plt.show()
+plt.close()
+
+# calculates and plots KDE for mass, with bandwidth of 3
+year_and_mass = year_and_mass.sort_values(by="mass")
+kde = scipy.stats.gaussian_kde(year_and_mass.mass, bw_method = 3)
+plt.plot(year_and_mass.mass, kde(year_and_mass.mass))
+plt.title("Distribution of Meteorite Mass")
+plt.xlabel("Mass (lbs)")
+plt.ylabel("Probability Density")
+plt.show()
+plt.close()
